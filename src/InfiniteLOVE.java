312e6a3794cf02e910ca2901f8aa758eb27268ff
AdamRXT <58094543+AdamRXT@users.noreply.github.com>
2019-11-23 03:58:56 +0100

    Create InfiniteLOVE.java

---
+++ src/InfiniteLOVE.java
@@ -0,0 +1,12 @@
+    public class EternalLOVE {
+
+        public static void main(final String[] args) { // main method
+        
+            boolean always;
+            always = TRUE;
+                do {
+                    LuvUalways();
+                } while (always);
+            }
+
+        }
